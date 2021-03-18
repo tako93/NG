06df7016823e4692cc030673bc3e3a66b6708d2f
Tako <73486278+tako93@users.noreply.github.com>
2021-03-18 20:18:55 +0400

    signin

--- intro-app/src/app/auth.guard.ts
+++ intro-app/src/app/auth.guard.ts
@@ -1,25 +1,32 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
 import { Observable } from 'rxjs';
 
+import { AuthService } from './auth/auth.service';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private _router: Router) {
-    
-  }
-  
+  constructor(private _router: Router, private authService: AuthService) {}
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): |Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-   
-    if (!localStorage.getItem('token')) {
-      this._router.navigate(['/auth/sign-in']);
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (!this.authService.isAuthorized()) {
+      this._router.navigate(['auth/sign-in']);
     }
-   
-    
+
     return true;
   }
-
 }
--- intro-app/src/app/auth/sign-in/sign-in.component.ts
+++ intro-app/src/app/auth/sign-in/sign-in.component.ts
@@ -1,32 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { SignInData, SignInResponse } from 'src/app/data/sign-in-form.interface';
+import { SignInData } from 'src/app/data/sign-in-form.interface';
 import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent implements OnInit {
-
   signInData: SignInData = {
-    email: "eve.holt@reqres.in",
-    password: "cityslicka",
-    remember: false, //ფორმაში არსებული პარამეტ. ობიექტი
-  }
-  constructor(private router: Router, private _authService: AuthService) { } //სერვისიდან
+    email: 'eve.holt@reqres.in',
+    password: 'cityslicka',
+    remember: false,
+  };
+  constructor(private router: Router, private _authService: AuthService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-   onSubmit(signInForm: NgForm) {
+  onSubmit(signInForm: NgForm) {
     this._authService
       .signIn(this.signInData)
-      .subscribe((response: SignInResponse) => {
-        console.log(response);
-        // this.router.navigate(['/auth/dashboard']);
+      .subscribe((isAuthorized: boolean) => {
+        // console.log(isAuthorized);
+        if (isAuthorized) {
+          this.router.navigate(['auth/dashboard']);
+        }
       });
   }
 }
