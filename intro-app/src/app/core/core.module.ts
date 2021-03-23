ebdd1d816ba46f80b61930c707c092ddf17c76b9
Tako <73486278+tako93@users.noreply.github.com>
2021-03-23 22:11:17 +0400

    refreshtoken& userrole

--- intro-app/src/app/core/core.module.ts
+++ intro-app/src/app/core/core.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { RouterModule } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component'
 
 
 @NgModule({
   declarations: [
-   
+   NavigationComponent
     
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
-   
+   NavigationComponent
   ]
 })
 export class CoreModule { }
