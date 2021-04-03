62211ffd975f08a61847c519680d73c7bcaac478
Tako <73486278+tako93@users.noreply.github.com>
2021-04-03 14:31:48 +0400

    signIn

--- intro-app/src/app/auth/shared/firebase-auth.service.ts
+++ intro-app/src/app/auth/shared/firebase-auth.service.ts
@@ -2,41 +2,46 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import  firebase  from 'firebase/app';
+import firebase from 'firebase/app';
 import { SignInData } from 'src/app/data/sign-in-form.interface';
-
 import { SignUpData } from 'src/app/data/sign-up-form.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FirebaseAuthService {
-
   currentUser$ = new Observable<firebase.User | null>();
+  hasError: string | null;
+
   constructor(private router: Router, private afAuth: AngularFireAuth) {
     this.currentUser$ = this.afAuth.authState;
+    this.hasError = null;
   }
+
   googleSignIn() {
-    this.afAuth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+    this.afAuth
+      .signInWithPopup(new firebase.auth.GoogleAuthProvider())
       .then(() => {
-      this.router.navigate(['/home'])
-      }).catch(error => {
-      console.error('[FirebaseAuthService]', error)
-    });
+        this.router.navigate(['/home']);
+      })
+      .catch((error) => {
+        console.log('[FirebaseAuthService@googleSignIn]', error);
+      });
   }
 
   signIn(data: SignInData) {
     this.afAuth
-      .createUserWithEmailAndPassword(data.email, data.password)
+      .signInWithEmailAndPassword(data.email, data.password)
       .then(() => {
         this.router.navigate(['/home']);
       })
       .catch((error) => {
         console.log('[FirebaseAuthService@signIn]', error);
+        this.hasError = error.message;
       });
   }
 
-   signUp(data: SignUpData) {
+  signUp(data: SignUpData) {
     return this.afAuth
       .createUserWithEmailAndPassword(data.email, data.password)
       .then((user) => {
@@ -44,15 +49,12 @@ export class FirebaseAuthService {
       })
       .catch((error) => {
         console.log('[FirebaseAuthService@signUp]', error);
+        this.hasError = error.message;
       });
   }
 
   signOut() {
     this.afAuth.signOut();
-    this.router.navigate(['/home'])
+    this.router.navigate(['/home']);
   }
-
 }
-
-
-
--- intro-app/src/app/auth/sign-in/sign-in.component.ts
+++ intro-app/src/app/auth/sign-in/sign-in.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { GuardsCheckStart, Router } from '@angular/router';
+import { FormControl, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { LocalStorageService } from 'src/app/core/local-storage.service';
 import { SignInData } from 'src/app/data/sign-in-form.interface';
 import { TOKEN_KEY } from 'src/app/shared/constants';
 import { AuthService } from '../shared/auth.service';
 import { FirebaseAuthService } from '../shared/firebase-auth.service';
-// import { User } from '../../data/user-data.interface';
-// import { UserRole } from '../../data/user-roles.interface';
 
 @Component({
   selector: 'app-sign-in',
@@ -15,64 +13,30 @@ import { FirebaseAuthService } from '../shared/firebase-auth.service';
   styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent implements OnInit {
-  // private activeUser: User = {
-  //   firstName: '',
-  //   lastName: '',
-  //   email: '',
-  //   avatar: '',
-  //   role: UserRole.guest,
-  // 'eve.holt@reqres.in','cityslicka',}
   signInData: SignInData = {
-    email: "eve.holt@reqres.in",
-    password: "cityslicka",
+    email: '',
+    password: '',
     remember: false,
   };
-  constructor(
-    private router: Router,
-    private _authService: AuthService,
-    private storageService: LocalStorageService,
-    private fireAuthService: FirebaseAuthService,
-  ) { }
 
-  ngOnInit(): void { }
+  mouseOverSignInButton: boolean = false;
 
-  async onSubmit(signInForm: NgForm) {
-    // this._authService
-    //   .signIn(this.signInData)
-    //   .subscribe((isAuthorized: boolean) => {
-    //       if (this._authService.redirectUrl != '') {
-    //        this.router.navigateByUrl(this._authService.redirectUrl);
-    //       } else {
-    //         this.router.navigate(['auth/dashboard']);
+  constructor(public fireAuthService: FirebaseAuthService) {}
 
-    //     }
-    //   });
+  ngOnInit(): void {}
 
-
-    // try {
-    //   const result = await this._authService.firebaseSignIn(this.signInData);
-
-    //   const IdTokenResult = await result.user?.getIdTokenResult();
-    //   // result.user?.refreshToken
-    //   if (IdTokenResult?.token) {
-    //     this.storageService.set(TOKEN_KEY, IdTokenResult.token);
-    //     if (this._authService.redirectUrl != '') {
-    //       this.router.navigateByUrl(this._authService.redirectUrl);
-    //     } else {
-    //       this.router.navigate(['auth/dashboard']);
-
-    //     }
-    //   }
-    // } catch (err) {
-    //   console.log(err)
-    // }
-
-
-    this.fireAuthService.signIn(this.signInData)
+  onSubmit(signInForm: SignInData) {
+    this.fireAuthService.hasError = null;
+    this.fireAuthService.signIn(signInForm);
   }
 
   signInWithGoogle() {
-    this.fireAuthService.googleSignIn()
+    this.fireAuthService.hasError = null;
+    this.fireAuthService.googleSignIn();
+  }
+
+  toggleMouserOver(value: boolean) {
+    this.mouseOverSignInButton = value;
   }
 }
- //ფორმაში შექმნილი პარამეტ.
+//ფორმაში შექმნილი პარამეტ.
